e598dfc10b2ec5257108d6d5e86402eb8cb55c1c
Chukwudobe Micah <micah@refine.agency>
2024-07-05 17:36:44 +0100

    chore: created yatch preview and details page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,62 +18,60 @@ import { ethers } from "ethers";
 import { SwapContextProvider } from "../cost/SwapContextBNB";
 import { SwapContextProviderETH } from "../cost/SwapContextETH";
 import NextNProgress from "nextjs-progressbar";
-
+import "styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
- 
   const [selectedChain, setSelectedChain] = useState("binance");
   return (
-    
-   <ChainContext.Provider value={{ selectedChain, setSelectedChain }}>
-    <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      secretKey={process.env.NEXT_PUBLIC_TEMPLATE_SECRET_KEY}
-      activeChain={selectedChain}
-      supportedWallets={[
-        metamaskWallet(),
-        coinbaseWallet(),
-        walletConnect(),
-        trustWallet(),
-        embeddedWallet({
-          auth: {
-            options: ["email", "google", "facebook", "apple"],
-          },
-        }),
-      ]}
-      sdkOptions={{
-        gatewayUrls: ["https://ipfs.io/ipfs/"],
-      }}
-    >
-      <ChakraProvider>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=5, minimum-scale=1, viewport-fit=cover"
-          />
-          <meta
-            name="description"
-            content="Osean is a crypto currency project designed to invest in Yachting industry"
-          />
-          <meta name="theme-color" content="#1FC7D4" />
-          <meta
-            name="twitter:image"
-            content="https://osean.online/osean200.png"
-          />
-          <meta
-            name="twitter:description"
-            content="Osean is a crypto currency project designed to invest in Yachting industry"
-          />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta
-            name="twitter:title"
-            content="🌊 OSEAN DAO - Osean DAO dapp"
-          />
-          <title>Osean DAO</title>
-        </Head>
-        <OseanHeaderLinks />
-        <OseanHeader />
-        {/* Progress bar when navigating between pages */}
+    <ChainContext.Provider value={{ selectedChain, setSelectedChain }}>
+      <ThirdwebProvider
+        clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+        secretKey={process.env.NEXT_PUBLIC_TEMPLATE_SECRET_KEY}
+        activeChain={selectedChain}
+        supportedWallets={[
+          metamaskWallet(),
+          coinbaseWallet(),
+          walletConnect(),
+          trustWallet(),
+          embeddedWallet({
+            auth: {
+              options: ["email", "google", "facebook", "apple"],
+            },
+          }),
+        ]}
+        sdkOptions={{
+          gatewayUrls: ["https://ipfs.io/ipfs/"],
+        }}
+      >
+        <ChakraProvider>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, maximum-scale=5, minimum-scale=1, viewport-fit=cover"
+            />
+            <meta
+              name="description"
+              content="Osean is a crypto currency project designed to invest in Yachting industry"
+            />
+            <meta name="theme-color" content="#1FC7D4" />
+            <meta
+              name="twitter:image"
+              content="https://osean.online/osean200.png"
+            />
+            <meta
+              name="twitter:description"
+              content="Osean is a crypto currency project designed to invest in Yachting industry"
+            />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta
+              name="twitter:title"
+              content="🌊 OSEAN DAO - Osean DAO dapp"
+            />
+            <title>Osean DAO</title>
+          </Head>
+          <OseanHeaderLinks />
+          <OseanHeader />
+          {/* Progress bar when navigating between pages */}
           <NextNProgress
             color="var(--color-tertiary)"
             startPosition={0.3}
@@ -81,15 +79,15 @@ function MyApp({ Component, pageProps }: AppProps) {
             height={3}
             showOnShallow={true}
           />
-        <SwapContextProvider>
-        <SwapContextProviderETH>
-        <Component {...pageProps} />
-        </SwapContextProviderETH>
-        </SwapContextProvider>
-        <OseanFooter />
-      </ChakraProvider>
-    </ThirdwebProvider>
-  </ChainContext.Provider>
+          <SwapContextProvider>
+            <SwapContextProviderETH>
+              <Component {...pageProps} />
+            </SwapContextProviderETH>
+          </SwapContextProvider>
+          <OseanFooter />
+        </ChakraProvider>
+      </ThirdwebProvider>
+    </ChainContext.Provider>
   );
 }
 
---
+++ pages/yacht-details/[slug].tsx
@@ -0,0 +1,25 @@
+// import { vendorData } from "public/data/listing-details";
+// import RelatedListingBlock from "@/components/listing-details/related-listings/related-listings-block";
+// import ListingDetails from "@/components/listing-details/listing-details-block";
+// import SubscriptionBlock from "@/components/subscription/subscription-block";
+
+import { vendorData } from "../../data/listing-details";
+import GallaryBlock from "../../components/GallaryBlock/GallaryBlock";
+import SubscriptionBlock from "../../components/SubscriptionBlock/SubscriptionBlock";
+import RelatedListingBlock from "../../components/RelatedListingBlock/RelatedListingBlock";
+// import ListingDetails from "../../components/ListingDetails/ListindDetails";
+
+export default function ListingDetailsPage() {
+  // const { slug } = params;
+
+  return (
+    <>
+      <div className="container-fluid pt-20 w-full 3xl:!px-12">
+        <GallaryBlock images={vendorData.gallary} />
+        {/* <ListingDetails    /> */}
+        <RelatedListingBlock />
+      </div>
+      <SubscriptionBlock sectionClassName="3xl:!px-12 4xl:!px-12" />
+    </>
+  );
+}
