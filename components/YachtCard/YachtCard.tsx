273fcc4e8d141b1dbfbcd5a19ec72ef9b8ed9634
Chukwudobe Micah <micah@refine.agency>
2024-07-20 01:29:36 +0100

    chore: fixed swiper nav buttons

--- components/YachtCard/YachtCard.tsx
+++ components/YachtCard/YachtCard.tsx
@@ -44,59 +44,72 @@ export default function YachtCard({
           isWishListed={false}
           onClick={(data) => console.log("Item added to Wishlist.", data)}
         /> */}
-        <Link href={`/yacht-details/${id}`}>
-          <div className="listing-item after:absolute after:bottom-0 after:left-0 after:z-[1] after:h-1/4 after:w-full after:bg-gradient-to-t after:from-black/25">
-            <Swiper
-              className="!static"
-              modules={[Pagination, Navigation]}
-              pagination={{
-                clickable: true,
-              }}
-              slidesPerView={1}
-              navigation={{
-                nextEl: `.boat_${id}-listing-item-button-next`,
-                prevEl: `.boat_${id}-listing-item-button-prev`,
-              }}
-            >
-              {slides?.map((slide, index) => (
-                <SwiperSlide key={`slide-${index}`}>
-                  <Image
-                    className="aspect-[34/25] bg-gray-lighter"
-                    src={slide}
-                    width={816}
-                    height={600}
-                    alt="boat"
-                    priority
-                  />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-            <ActionIcon
-              rounded="full"
-              color="light"
-              size="sm"
-              className={clsx(
-                "absolute left-4 top-1/2 z-10 hidden -translate-y-1/2 shadow-md !transition-all focus:!ring-0 md:invisible md:flex md:disabled:hidden md:group-hover/item:visible",
-                `${id}-listing-item-button-prev`
-              )}
-            >
-              <ChevronLeftIcon className="-ml-0.5 h-auto w-[7px]" />
-            </ActionIcon>
-            <ActionIcon
-              rounded="full"
-              size="sm"
-              color="light"
-              className={clsx(
-                "absolute right-4 top-1/2 z-10 hidden -translate-y-1/2 opacity-80 shadow-md !transition-all duration-300 focus:!ring-0 md:invisible md:flex md:disabled:hidden md:group-hover/item:visible md:group-hover/item:opacity-100",
-                `${id}-listing-item-button-next`
-              )}
-            >
-              <ChevronRightIcon className="-mr-0.5 h-auto w-[7px]" />
-            </ActionIcon>
+        <div className="listing-item after:absolute after:bottom-0 after:left-0 after:z-[1] after:h-1/4 after:w-full after:bg-gradient-to-t after:from-black/25">
+          <Swiper
+            className="!static"
+            modules={[Pagination, Navigation]}
+            pagination={{
+              clickable: true,
+            }}
+            loop
+            slidesPerView={1}
+            navigation={{
+              nextEl: `.boat_${id}-listing-item-button-next`,
+              prevEl: `.boat_${id}-listing-item-button-prev`,
+            }}
+          >
+            {slides?.map((slide, index) => (
+              <SwiperSlide key={`slide-${index}`}>
+                <Image
+                  className="aspect-[34/25] bg-gray-lighter"
+                  src={slide}
+                  width={816}
+                  height={600}
+                  alt="boat"
+                  priority
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          {/* <ActionIcon
+            rounded="full"
+            color="light"
+            size="sm"
+            className={clsx(
+              ` boat_${id}-listing-item-button-prev absolute left-4 top-1/2 z-10 hiden -translate-y-1/2 shadow-md !transition-all focus:!ring-0 md:invsible md:flex md:disable:hidden md:group-hover/item:isible`
+            )}
+          >
+            <ChevronLeftIcon className="-ml-0.5 h-auto w-[7px]" />
+          </ActionIcon> */}
+          <div
+            onClick={() => {
+              console.log("I was clicked");
+            }}
+            className={clsx(
+              `!border-gray-200 !bg-gray-100 !text-gray-400 cursor-pointer px-2.5 py-2 aspect-square rounded-full boat_${id}-listing-item-button-prev absolute left-4 top-1/2 z-10 -translate-y-1/2 opacity-80 shadow-md !transition-all duration-300 focus:!ring-0 items-center justify-center md:flex md:group-hover/item:opacity-100`
+            )}
+          >
+            <div>
+              <ChevronLeftIcon className="-mr-0.5 text-black h-auto w-[7px]" />
+            </div>{" "}
           </div>
-        </Link>
+          {/*  */}
+          <div
+            onClick={() => {
+              console.log("I was clicked");
+            }}
+            className={clsx(
+              ` !border-gray-200 !bg-gray-100 !text-gray-400 cursor-pointer px-2.5 py-2 aspect-square rounded-full boat_${id}-listing-item-button-next absolute right-4 top-1/2 z-10 -translate-y-1/2 opacity-80 shadow-md !transition-all duration-300 focus:!ring-0 items-center justify-center md:flex md:group-hover/item:opacity-100`
+            )}
+          >
+            <div>
+              <ChevronRightIcon className="-mr-0.5 text-black h-auto w-[7px]" />
+            </div>{" "}
+          </div>
+        </div>
       </div>
-      <Link href={"/"}>
+      <Link href={`/yacht-details/${id}`}>
+        {/* // <Link href={"/"}> */}
         <div className="content pt-3 text-black">
           <div className="mb-1 flex items-center gap-5">
             {/* <span className="relative flex items-center font-bold text-gray-dark before:absolute before:-right-3 before:block before:h-1 before:w-1 before:rounded-full before:bg-gray-dark">
